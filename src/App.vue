ce0682ccc9cd76b6de20e3a81c8fde0f21055cca
Pherp Guy <pherpguy@gmail.com>
2018-04-09 23:41:05 +0100

    working connection with firebase, removed config

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="panel-body">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="book in books">
+            <td><a v-bind:href="book.url">{{book.title}}</a></td>
+            <td>{{book.author}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+import Firebase from 'firebase'
+
+let config = {
+
+  };
+
+  
+let app = Firebase.initializeApp(config)
+let db = app.database()
+
+let booksRef = db.ref('books')
+
+export default {
+  name: 'app',
+  firebase: {
+    books: booksRef
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
